ac8446cf612a9a8beebed4e6cd70ef0cec5177db
Oleksandr Tverdokhlib <tverdokhlib@gmail.com>
2021-04-28 14:23:15 +0300

    Applied src:fix

--- src/test/csv-transformer-test.ts
+++ src/test/csv-transformer-test.ts
@@ -1,5 +1,5 @@
 import { rotateColumns } from '../transformers/csv-transformer';
-import { Describe, Test } from 'jest-decorator'
+import { Describe, Test } from 'jest-decorator';
 
 @Describe('Transformer Test')
 class CSVTransformerTest {
@@ -7,7 +7,7 @@ class CSVTransformerTest {
   @Test('CSV columns rotated one position')
   private rotateColumsOnePosition () {
     done => {
-        function callback(error, data) {
+      function callback(error, data) {
         try {
           expect(data).toStrictEqual(['0.34', 'af001']);
           done();
@@ -16,13 +16,13 @@ class CSVTransformerTest {
         }
       }
       rotateColumns(['af001', '0.34'], callback);
-    }
+    };
   }
 
   @Test('CSV columns rotated one position, one of the values is empty')
   private rotateColumsOnePositionEmptyValue () {
     done => { 
-        function callback(error, data) {
+      function callback(error, data) {
         try {
           expect(data).toStrictEqual(['0.34', '']);
           done();
@@ -31,6 +31,6 @@ class CSVTransformerTest {
         }
       } 
       rotateColumns(['', '0.34'], callback);
-    }
+    };
   }
 }
